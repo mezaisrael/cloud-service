9984dad556a17d1482cac212e57e9e43272a235a
mezaisrael <meza.israel.91@gmail.com>
2019-04-30 22:08:52 -0400

    Add URB user interface

---
+++ urbUI/main.js
@@ -0,0 +1,14 @@
+let currentJObs = [];
+
+let currentJobs = () => {
+	let time = document.getElementById("time-col");
+
+	time.innerHTML = "Time Left";
+}
+
+
+let showHistory = () => {
+	let time = document.getElementById("time-col");
+
+	time.innerHTML = "End Time";
+}
